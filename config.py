955afce23bee61c14d384093e730604bc989070c
yarnav <51213596+yarnav@users.noreply.github.com>
2023-08-16 00:35:22 +0530

    Config & Python Script to produce to Kafka topic
    Configuration and Main Python script that gets list of videos and their details from Youtube Playlist(Used Google PlaylistItems API) and produce these details to Confluent Kafka via Avro Schema.

---
+++ config.py
@@ -0,0 +1,15 @@
+config = {
+    "google_api_key": "AIzaSyD5nnC6biEu92xMr-y4mDukvEW6qoaBuFw",
+    "youtube_playlist_id": "PLr3-bEi1N4khKZm2u6Q8pOLO-WNZrKDHn",
+    "kafka": {
+        "bootstrap.servers": "pkc-xrnwx.asia-south2.gcp.confluent.cloud:9092",
+        "security.protocol": "sasl_ssl",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "OBOZEJN4SWE27M2P",
+        "sasl.password": "ZdSN8yi90JPqrhXCOBl5UpiiPHFyZYitABuqXbE9/O6vEXuEVIer97rPBj5aHBWu",
+    },
+    "schema_registry": {
+        "url": "https://psrc-10wzj.ap-southeast-2.aws.confluent.cloud",
+        "basic.auth.user.info": "SE7QAQMXGGDEBQUM:Nf8+wSiQgsJX10UnIOhk+2TfY7jVCOqjogS+BExf5ugOxw1aZQJE1SXdDVK3tD02",
+        }
+}
